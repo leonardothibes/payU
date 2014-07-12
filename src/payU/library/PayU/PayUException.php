c6ae3687d4c9a8391e2a46d6600f12b5962e69e8
Leonardo Thibes <eu@leonardothibes.com>
2014-07-12 18:29:01 -0300

    identation only

---
+++ src/payU/library/PayU/PayUException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace PayU;
+
+/**
+ * API main exception.
+ *
+ * @package PayU
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class PayUException extends \Exception
+{
+    public function test()
+    {
+
+    }
+}
