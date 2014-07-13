c05cac71fc9b307eb00031f8d12fab382d06af16
Leonardo Thibes <eu@leonardothibes.com>
2014-07-13 00:30:39 -0300

    identation only

--- src/PayU/PayUVersion.php
+++ src/PayU/PayUVersion.php
@@ -15,22 +15,22 @@ namespace PayU;
  */
 class PayUVersion
 {
-	/**
-	 * Get location of version number file.
-	 * @return string
-	 */
-	public static function getVersionFileLocation()
-	{
-		return realpath(dirname(__FILE__) . '/Version/Number.txt');
-	}
+    /**
+     * Get location of version number file.
+     * @return string
+     */
+    public static function getVersionFileLocation()
+    {
+        return realpath(dirname(__FILE__) . '/Version/Number.txt');
+    }
 
-	/**
-	 * Get the current version number.
-	 * @return string
-	 */
-	public static function getCurrent()
-	{
-		$file = self::getVersionFileLocation();
-		return trim(file_get_contents($file), '\t\n\r');
-	}
+    /**
+     * Get the current version number.
+     * @return string
+     */
+    public static function getCurrent()
+    {
+        $file = self::getVersionFileLocation();
+        return trim(file_get_contents($file), '\t\n\r');
+    }
 }
