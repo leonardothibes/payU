8c24c0bea58a19b85eb3d9e60c4d378d3a37173a
Leonardo Thibes <leonardothibes@gmail.com>
2014-07-17 15:08:54 -0300

    implementing constants file for payment cuntries

---
+++ src/PayU/Payment/PaymentCountries.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace PayU\Payment;
+
+/**
+ * Constants for supported payment cuntries.
+ *
+ * @package PayU\Payment
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+class PaymentCountries
+{
+	const BRAZIL    = 'BR';
+	const ARGENTINA = 'AR';
+	const PANAMA    = 'PA';
+	const MEXICO    = 'ME';
+	const COLOMBIA  = 'CO';
+	const PERU      = 'PE';
+}
