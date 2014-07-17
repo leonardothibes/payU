b64f7a12a06dd3d6064a2f123c67fbf96a2abf85
Leonardo Thibes <leonardothibes@gmail.com>
2014-07-17 15:00:56 -0300

    implementing constants file for payment methods

--- src/PayU/Payment/PaymentMethods.php
+++ src/PayU/Payment/PaymentMethods.php
@@ -22,7 +22,7 @@ class PaymentMethods
 	 * @country Mexico
 	 * @country Colombia
 	 */
-	const VISA   = 'VISA';
+	const VISA = 'VISA';
 
 	/**
 	 * @country Brazil
@@ -30,282 +30,64 @@ class PaymentMethods
 	 * @country Panama
 	 * @country Mexico
 	 * @country Colombia
+	 * @country Peru
 	 */
-	const MASTER = 'MASTERCARD';
+	const MASTERCARD = 'MASTERCARD';
 
-	const DINNRS = 'DINERS';
+	/**
+	 * @country Brazil
+	 * @country Colombia
+	 */
+	const DINERS = 'DINERS';
 
-	[0] => stdClass Object
-	(
-			[id] => 167
-			[description] => AMEX
-			[country] => BR
-	)
+	/**
+	 * @country Brazil
+	 * @country Argentina
+	 * @country Colombia
+	 * @country Mexico
+	 */
+	const AMEX = 'AMEX';
 
-	[1] => stdClass Object
-	(
-			[id] => 163
-			[description] => DINERS
-			[country] => BR
-	)
+	/**
+	 * @country Brazil
+	 */
+	const BOLETO_BANCARIO = 'BOLETO_BANCARIO';
+	const ELO             = 'ELO';
 
-	[2] => stdClass Object
-	(
-			[id] => 160
-			[description] => BOLETO_BANCARIO
-			[country] => BR
-	)
+	/**
+	 * @country Argentina
+	 */
+	const ARGENCARD     = 'ARGENCARD';
+	const BAPRO         = 'BAPRO';
+	const CABAL         = 'CABAL';
+	const COBRO_EXPRESS = 'COBRO_EXPRESS';
+	const NARANJA       = 'NARANJA';
+	const PAGOFACIL     = 'PAGOFACIL';
+	const RAPIPAGO      = 'RAPIPAGO';
+	const RIPSA         = 'RIPSA';
+	const SHOPPING      = 'SHOPPING';
 
-	[3] => stdClass Object
-	(
-			[id] => 162
-			[description] => VISA
-			[country] => BR
-	)
+	/**
+	 * @country Mexico
+	 */
+	const BANCOMER     = 'BANCOMER';
+	const IXE          = 'IXE';
+	const OXXO         = 'OXXO';
+	const SANTANDER    = 'SANTANDER';
+	const SCOTIABANK   = 'SCOTIABANK';
+	const SEVEN_ELEVEN = 'SEVEN_ELEVEN';
 
-	[4] => stdClass Object
-	(
-			[id] => 164
-			[description] => MASTERCARD
-			[country] => BR
-	)
 
-	[5] => stdClass Object
-	(
-			[id] => 166
-			[description] => ELO
-			[country] => BR
-	)
+	/**
+	 * @country Colombia
+	 */
+	const ACH_DEBIT       = 'ACH_DEBIT';
+	const BALOTO          = 'BALOTO';
+	const BANK_REFERENCED = 'BANK_REFERENCED';
+	const PSE             = 'PSE';
 
-	[0] => stdClass Object
-	(
-			[id] => 195
-			[description] => VISA
-			[country] => AR
-	)
-
-	[1] => stdClass Object
-	(
-			[id] => 191
-			[description] => PAGOFACIL
-			[country] => AR
-	)
-
-	[2] => stdClass Object
-	(
-			[id] => 208
-			[description] => RIPSA
-			[country] => AR
-	)
-
-	[3] => stdClass Object
-	(
-			[id] => 196
-			[description] => MASTERCARD
-			[country] => AR
-	)
-
-	[4] => stdClass Object
-	(
-			[id] => 302
-			[description] => BANCOMER
-			[country] => MX
-	)
-
-	[5] => stdClass Object
-	(
-			[id] => 197
-			[description] => AMEX
-			[country] => AR
-	)
-
-	[6] => stdClass Object
-	(
-			[id] => 100
-			[description] => BCP
-			[country] => PE
-	)
-
-	[7] => stdClass Object
-	(
-			[id] => 254
-			[description] => PSE
-			[country] => CO
-	)
-
-	[8] => stdClass Object
-	(
-			[id] => 251
-			[description] => MASTERCARD
-			[country] => CO
-	)
-
-	[9] => stdClass Object
-	(
-			[id] => 252
-			[description] => AMEX
-			[country] => CO
-	)
-
-	[10] => stdClass Object
-	(
-			[id] => 304
-			[description] => IXE
-			[country] => MX
-	)
-
-	[11] => stdClass Object
-	(
-			[id] => 305
-			[description] => SANTANDER
-			[country] => MX
-	)
-
-	[12] => stdClass Object
-	(
-			[id] => 220
-			[description] => VISA
-			[country] => PA
-	)
-
-	[13] => stdClass Object
-	(
-			[id] => 199
-			[description] => NARANJA
-			[country] => AR
-	)
-
-	[14] => stdClass Object
-	(
-			[id] => 222
-			[description] => MASTERCARD
-			[country] => PA
-	)
-
-	[15] => stdClass Object
-	(
-			[id] => 139
-			[description] => AMEX
-			[country] => MX
-	)
-
-	[16] => stdClass Object
-	(
-			[id] => 193
-			[description] => COBRO_EXPRESS
-			[country] => AR
-	)
-
-	[17] => stdClass Object
-	(
-			[id] => 260
-			[description] => VISA
-			[country] => MX
-	)
-
-	[18] => stdClass Object
-	(
-			[id] => 200
-			[description] => CABAL
-			[country] => AR
-	)
-
-	[19] => stdClass Object
-	(
-			[id] => 198
-			[description] => SHOPPING
-			[country] => AR
-	)
-
-	[20] => stdClass Object
-	(
-			[id] => 192
-			[description] => BAPRO
-			[country] => AR
-	)
-
-	[21] => stdClass Object
-	(
-			[id] => 253
-			[description] => DINERS
-			[country] => CO
-	)
-
-	[22] => stdClass Object
-	(
-			[id] => 306
-			[description] => SCOTIABANK
-			[country] => MX
-	)
-
-	[23] => stdClass Object
-	(
-			[id] => 190
-			[description] => RAPIPAGO
-			[country] => AR
-	)
-
-	[24] => stdClass Object
-	(
-			[id] => 102
-			[description] => MASTERCARD
-			[country] => PE
-	)
-
-	[25] => stdClass Object
-	(
-			[id] => 26
-			[description] => ACH_DEBIT
-			[country] => CO
-	)
-
-	[26] => stdClass Object
-	(
-			[id] => 130
-			[description] => SEVEN_ELEVEN
-			[country] => MX
-	)
-
-	[27] => stdClass Object
-	(
-			[id] => 250
-			[description] => VISA
-			[country] => CO
-	)
-
-	[28] => stdClass Object
-	(
-			[id] => 131
-			[description] => OXXO
-			[country] => MX
-	)
-
-	[29] => stdClass Object
-	(
-			[id] => 35
-			[description] => BALOTO
-			[country] => CO
-	)
-
-	[30] => stdClass Object
-	(
-			[id] => 261
-			[description] => MASTERCARD
-			[country] => MX
-	)
-
-	[31] => stdClass Object
-	(
-			[id] => 36
-			[description] => BANK_REFERENCED
-			[country] => CO
-	)
-
-	[32] => stdClass Object
-	(
-			[id] => 201
-			[description] => ARGENCARD
-			[country] => AR
-	)
-
+	/**
+	 * @country Peru
+	 */
+	const BCP = 'BCP';
 }
