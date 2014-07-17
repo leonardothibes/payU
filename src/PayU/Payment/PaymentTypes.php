049e8f832abd572845b0f9111fb9c5f31f4ae65c
Leonardo Thibes <leonardothibes@gmail.com>
2014-07-17 15:13:11 -0300

    implementing constants file for payment types

---
+++ src/PayU/Payment/PaymentTypes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace PayU\Payment;
+
+/**
+ * Constants for payment types supported by api.
+ *
+ * @package PayU\Payment
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+class PaymentTypes
+{
+	const AUTHORIZATION_AND_CAPTURE = 'AUTHORIZATION_AND_CAPTURE';
+	const AUTHORIZATION             = 'AUTHORIZATION';
+}
