5b9b2607afaea03a66becc9854f6822910e82039
Leonardo Thibes <leonardothibes@gmail.com>
2014-07-22 18:12:14 -0300

    implementing entities classes

--- src/PayU/Entity/Transaction/ExtraParametersEntity.php
+++ src/PayU/Entity/Transaction/ExtraParametersEntity.php
@@ -7,6 +7,7 @@
 namespace PayU\Entity\Transaction;
 
 use \PayU\Entity\EntityInterface;
+use \PayU\Entity\EntityException;
 
 /**
  * Extra parameters entity class.
@@ -18,11 +19,169 @@ use \PayU\Entity\EntityInterface;
 class ExtraParametersEntity implements EntityInterface
 {
     /**
+     * Installments number.
+     * @var int
+     */
+    protected $installmentsNumber = 1;
+
+    /**
+     * Set installments number.
+     *
+     * @param  int $installmentsNumber
+     * @return ExtraParametersEntity
+     */
+    public function setInstallmentsNumber($installmentsNumber)
+    {
+        $this->installmentsNumber = (int)$installmentsNumber;
+        return $this;
+    }
+
+    /**
+     * Get installments number.
+     * @return int
+     */
+    public function getInstallmentsNumber()
+    {
+        return (int)$this->installmentsNumber;
+    }
+
+    /**
+     * Installments type options.
+     */
+    const INSTALLMENTS_NUMBER                 = 'INSTALLMENTS_NUMBER';
+    const INSTALLMENTS_TYPE                   = 'INSTALLMENTS_TYPE';
+    const INSTALLMENT_PAYMENT_ON_SITE         = 1;
+    const INSTALLMENT_BUSINESS_FUNDING        = 2;
+    const INSTALLMENT_PAYMENT_NETWORK_FUNDING = 3;
+
+    /**
+     * Installments type.
+     * @var int
+     */
+    protected $installmentsType = self::INSTALLMENT_PAYMENT_ON_SITE;
+
+    /**
+     * Set installments type.
+     *
+     * @param  int $installmentsType
+     * @return ExtraParametersEntity
+     * @throws EntityException
+     */
+    public function setInstallmentsType($installmentsType)
+    {
+        $acceptedTypes = array(
+            self::INSTALLMENT_PAYMENT_ON_SITE,
+            self::INSTALLMENT_BUSINESS_FUNDING,
+            self::INSTALLMENT_PAYMENT_NETWORK_FUNDING,
+        );
+        if (!in_array($installmentsType, $acceptedTypes)) {
+            throw new EntityException('Invalid installment type');
+        }
+        $this->installmentsType = (int)$installmentsType;
+        return $this;
+    }
+
+    /**
+     * Set installments type.
+     */
+    public function getInstallmentsType()
+    {
+        return (int)$this->installmentsType;
+    }
+
+    /**
+     * Security code indicator options.
+     */
+    const SECURITY_CODE_INDICATOR    = 'SECURITY_CODE_INDICATOR';
+    const SECURITY_CODE_NOT_PROVIDED = 0;
+    const SECURITY_CODE_PROVIDED     = 1;
+    const SECURITY_CODE_UNREADABLE   = 2;
+    const SECURITY_CODE_NON_EXISTENT = 9;
+
+    /**
+     * Security code indicator.
+     * @var int
+     */
+    protected $securityCodeIndicator = self::SECURITY_CODE_PROVIDED;
+
+    /**
+     * Set security code indicator.
+     *
+     * @param  int $securityCodeIndicator
+     * @return ExtraParametersEntity
+     * @throws EntityException
+     */
+    public function setSecurityCodeIndicator($securityCodeIndicator)
+    {
+        $acceptedCodes = array(
+            self::SECURITY_CODE_NOT_PROVIDED,
+            self::SECURITY_CODE_PROVIDED,
+            self::SECURITY_CODE_NON_EXISTENT,
+            self::SECURITY_CODE_UNREADABLE,
+        );
+        if (!in_array($securityCodeIndicator, $acceptedCodes)) {
+            throw new EntityException('Invalid code indicator');
+        }
+        $this->securityCodeIndicator = (int)$securityCodeIndicator;
+        return $this;
+    }
+
+    /**
+     * Get security code indicator.
+     * @return int
+     */
+    public function getSecurityCodeIndicator()
+    {
+        return (int)$this->securityCodeIndicator;
+    }
+
+    /**
+     * Response url option.
+     */
+    const RESPONSE_URL = 'RESPONSE_URL';
+
+    /**
+     * Response url.
+     * @var string
+     */
+    protected $responseUrl = null;
+
+    /**
+     * Set response url.
+     *
+     * @param  string $responseUrl
+     * @return ExtraParametersEntity
+     */
+    public function setResponseUrl($responseUrl)
+    {
+        if (substr($responseUrl, 0, 4) != 'http') {
+            $message = sprintf('Invalid url: %s', $responseUrl);
+            throw new EntityException($message);
+        }
+        $this->responseUrl = (string)$responseUrl;
+        return $this;
+    }
+
+    /**
+     * Get response url.
+     * @return string
+     */
+    public function getResponseUrl()
+    {
+        return (string)$this->responseUrl;
+    }
+
+    /**
      * Generate arry order.
      * @return array
      */
     public function toArray()
     {
-        return array();
+        return array(
+            self::INSTALLMENTS_NUMBER     => $this->installmentsNumber,
+            self::INSTALLMENTS_TYPE       => $this->installmentsType,
+            self::SECURITY_CODE_INDICATOR => $this->securityCodeIndicator,
+            self::RESPONSE_URL            => $this->responseUrl,
+        );
     }
 }
---
+++ tests/PayU/Entity/Transaction/ExtraParametersEntityTest.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace PayU\Entity\Transaction;
+use \PayU\Entity\Transaction\ExtraParametersEntity;
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+/**
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+class ExtraParametersEntityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ExtraParametersEntity
+     */
+    protected $object = null;
+
+    /**
+     * Setup.
+     */
+    protected function setUp()
+    {
+    	$this->object = new ExtraParametersEntity;
+    }
+
+    /**
+     * TearDown.
+     */
+    protected function tearDown()
+    {
+    	unset($this->object);
+    }
+
+    /**
+     * Testing constants values.
+     */
+    public function testConstantsValues()
+    {
+    	$this->assertEquals(ExtraParametersEntity::INSTALLMENTS_NUMBER                , 'INSTALLMENTS_NUMBER');
+    	$this->assertEquals(ExtraParametersEntity::INSTALLMENTS_TYPE                  , 'INSTALLMENTS_TYPE');
+    	$this->assertEquals(ExtraParametersEntity::INSTALLMENT_PAYMENT_ON_SITE        , 1);
+    	$this->assertEquals(ExtraParametersEntity::INSTALLMENT_BUSINESS_FUNDING       , 2);
+    	$this->assertEquals(ExtraParametersEntity::INSTALLMENT_PAYMENT_NETWORK_FUNDING, 3);
+    	$this->assertEquals(ExtraParametersEntity::SECURITY_CODE_INDICATOR            , 'SECURITY_CODE_INDICATOR');
+    	$this->assertEquals(ExtraParametersEntity::SECURITY_CODE_NOT_PROVIDED         , 0);
+    	$this->assertEquals(ExtraParametersEntity::SECURITY_CODE_PROVIDED             , 1);
+    	$this->assertEquals(ExtraParametersEntity::SECURITY_CODE_UNREADABLE           , 2);
+    	$this->assertEquals(ExtraParametersEntity::SECURITY_CODE_NON_EXISTENT         , 9);
+    	$this->assertEquals(ExtraParametersEntity::RESPONSE_URL                       , 'RESPONSE_URL');
+    }
+
+	/**
+	 * @see ExtraParametersEntity::getInstallmentsNumber()
+	 */
+    public function testGetInstallmentsNumber()
+    {
+    	$rs = $this->object->getInstallmentsNumber();
+    	$this->assertInternalType('int', $rs);
+    	$this->assertEquals(1, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setInstallmentsNumber()
+     */
+    public function testSetInstallmentsNumber()
+    {
+    	$installmentsNumber = rand(1, 10);
+    	$rs                 = $this->object->setInstallmentsNumber($installmentsNumber);
+    	$this->assertInstanceOf('\PayU\Entity\Transaction\ExtraParametersEntity', $rs);
+
+    	$rs = $this->object->getInstallmentsNumber();
+    	$this->assertInternalType('int', $rs);
+    	$this->assertEquals($installmentsNumber, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::getInstallmentsType
+     */
+    public function testGetInstallmentsType()
+    {
+    	$rs = $this->object->getInstallmentsType();
+    	$this->assertInternalType('int', $rs);
+    	$this->assertEquals(ExtraParametersEntity::INSTALLMENT_PAYMENT_ON_SITE, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setInstallmentsType()
+     */
+    public function testSetInstallmentsType()
+    {
+    	$installmentsType = rand(1, 3);
+    	$rs               = $this->object->setInstallmentsType($installmentsType);
+    	$this->assertInstanceOf('\PayU\Entity\Transaction\ExtraParametersEntity', $rs);
+
+    	$rs = $this->object->getInstallmentsType();
+    	$this->assertInternalType('int', $rs);
+    	$this->assertEquals($installmentsType, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setInstallmentsType()
+     * @expectedException \PayU\Entity\EntityException
+     */
+    public function testSetInstallmentsTypeException()
+    {
+    	$this->object->setInstallmentsType(4);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setSecurityCodeIndicator()
+     */
+    public function testGetSecurityCodeIndicator()
+    {
+    	$rs = $this->object->getSecurityCodeIndicator();
+    	$this->assertInternalType('int', $rs);
+    	$this->assertEquals(ExtraParametersEntity::SECURITY_CODE_PROVIDED, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setSecurityCodeIndicator()
+     */
+    public function testSetSecurityCodeIndicator()
+    {
+    	$securityCodeIndicator = rand(0, 2);
+    	$rs                    = $this->object->setSecurityCodeIndicator($securityCodeIndicator);
+    	$this->assertInstanceOf('\PayU\Entity\Transaction\ExtraParametersEntity', $rs);
+
+    	$rs = $this->object->getSecurityCodeIndicator();
+    	$this->assertInternalType('int', $rs);
+    	$this->assertEquals($securityCodeIndicator, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setSecurityCodeIndicator()
+     * @expectedException \PayU\Entity\EntityException
+     */
+    public function testSetSecurityCodeIndicatorException()
+    {
+    	$securityCodeIndicator = rand(3, 8);
+    	$this->object->setSecurityCodeIndicator($securityCodeIndicator);
+    }
+
+    /**
+     * @see ExtraParametersEntity::getResponseUrl()
+     */
+    public function testGetResponseUrl()
+    {
+    	$rs = $this->object->getResponseUrl();
+    	$this->assertInternalType('string', $rs);
+    	$this->assertEquals(0, strlen($rs));
+    }
+
+    /**
+     * @see ExtraParametersEntity::setResponseUrl()
+     */
+    public function testSetResponseUrl()
+    {
+    	$responseUrl = 'https://response-url-' . rand(1, 1000) . '.com';
+    	$rs          = $this->object->setResponseUrl($responseUrl);
+    	$this->assertInstanceOf('\PayU\Entity\Transaction\ExtraParametersEntity', $rs);
+
+    	$rs = $this->object->getResponseUrl();
+    	$this->assertInternalType('string', $rs);
+    	$this->assertEquals($responseUrl, $rs);
+    }
+
+    /**
+     * @see ExtraParametersEntity::setResponseUrl()
+     * @expectedException \PayU\Entity\EntityException
+     */
+    public function testSetResponseUrlException()
+    {
+    	$this->object->setResponseUrl('invalid-url-here');
+    }
+
+    /**
+     * @see ExtraParametersEntity::toArray()
+     */
+    public function testToArray()
+    {
+    	$installmentsNumber    = rand(1, 10);
+    	$installmentsType      = rand(1, 3);
+    	$securityCodeIndicator = rand(0, 2);
+    	$responseUrl           = 'https://response-url-' . rand(1, 1000) . '.com';
+
+    	$this->object->setInstallmentsNumber($installmentsNumber)
+    	             ->setInstallmentsType($installmentsType)
+    	             ->setSecurityCodeIndicator($securityCodeIndicator)
+    	             ->setResponseUrl($responseUrl);
+    	$rs = $this->object->toArray();
+
+    	$this->assertInternalType('array', $rs);
+    	$this->assertEquals($installmentsNumber   , $rs[ExtraParametersEntity::INSTALLMENTS_NUMBER]);
+    	$this->assertEquals($installmentsType     , $rs[ExtraParametersEntity::INSTALLMENTS_TYPE]);
+    	$this->assertEquals($securityCodeIndicator, $rs[ExtraParametersEntity::SECURITY_CODE_INDICATOR]);
+    	$this->assertEquals($responseUrl          , $rs[ExtraParametersEntity::RESPONSE_URL]);
+    }
+}
