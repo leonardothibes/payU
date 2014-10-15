9d4c1689de40dc510f7409bb80c9f99c896bbc1d
Leonardo Thibes <leonardothibes@gmail.com>
2014-10-15 14:37:36 -0300

    new currencies constant class: \PayU\Payment\PaymentCurrency

---
+++ src/PayU/Payment/PaymentCurrency.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace PayU\Payment;
+
+/**
+ * Constants for supported payment currencies.
+ *
+ * @package PayU\Payment
+ * @author Leonardo Thibes <leonardothibes@gmail.com>
+ * @copyright Copyright (c) The Authors
+ * @link http://www.xe.com/iso4217.php
+ */
+class PaymentCurrency
+{
+    const BRAZIL    = 'BRL';
+    const ARGENTINA = 'ARS';
+    const PANAMA    = 'USD';
+    const MEXICO    = 'MXN';
+    const COLOMBIA  = 'COP';
+    const PERU      = 'PEN';
+    const EUA       = 'USD';
+}
