5d10d6aa935a5592ae8c252d0415c6fea5339057
Leonardo Thibes <leonardothibes@gmail.com>
2015-04-15 11:15:38 -0300

    Adding configuration file for examples credentials

---
+++ examples/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Panama
+define('PAYU_CREDENTIAL_ACCOUNT_ID' , '500537');
+define('PAYU_CREDENTIAL_MERCHANT_ID', '500238');
+define('PAYU_CREDENTIAL_API_LOGIN'  , '11959c415b33d0c');
+define('PAYU_CREDENTIAL_API_KEY'    , '6u39nqhq8ftd0hlvnjfs66eh8c');
